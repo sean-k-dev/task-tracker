af28194b9d172d4120165cf34a06692744249054
Sean Kelly <seankwebdev@gmail.com>
2022-07-15 00:40:03 +0100

    Added checkbox and delete button, then set up props to raise events with the Container

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,17 @@ import React from "react"
 
 class TodoItem extends React.Component {
     render() {
-    return <li>{this.props.todo.title}</li>
+        return (
+        <li>
+            <input 
+                type="checkbox"
+                checked={this.props.todo.completed}
+                onChange={() => this.props.eventHandlerProps(this.props.todo.id)}
+            />
+            {this.props.todo.title}
+            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete Task</button>
+        </li>
+        )
     }
 }
 
