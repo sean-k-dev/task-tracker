5b346cf703ce97ae61e6e9ed6aacced65fa182fa
Sean Kelly <seankwebdev@gmail.com>
2022-07-16 20:49:07 +0100

    Updated TodoItem to set class names and apply styling to the list elements

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,26 @@
 import React from "react"
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
     render() {
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+          }
         return (
-        <li>
+        <li className={styles.item}>
             <input 
                 type="checkbox"
+                className={styles.checkbox}
                 checked={this.props.todo.completed}
                 onChange={() => this.props.eventHandlerProps(this.props.todo.id)}
             />
-            {this.props.todo.title}
             <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete Task</button>
+            <span style={this.props.todo.completed ? completedStyle : null}>
+                {this.props.todo.title}
+            </span>
         </li>
         )
     }
