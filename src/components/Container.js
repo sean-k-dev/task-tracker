8c5c771b0aeefcc6d7057961f42f52bd89c2fdbb
Sean Kelly <seankwebdev@gmail.com>
2022-07-17 18:08:01 +0100

    Added code to set state for edited todo tasks

--- src/components/Container.js
+++ src/components/Container.js
@@ -58,6 +58,16 @@ class Container extends React.Component {
             todos: [...this.state.todos, newTodo]
         })
     }
+    setUpdate = (updatedTitle, id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.title = updatedTitle
+                }
+            return todo
+            }),
+        })
+    }
     render() {
         return (
             <div className="container">
@@ -68,6 +78,7 @@ class Container extends React.Component {
                         todos={this.state.todos} 
                         eventHandlerProps={this.eventHandler}
                         deleteTodoProps={this.deleteTodo}
+                        setUpdate={this.setUpdate}
                     />
                 </div>
             </div>
