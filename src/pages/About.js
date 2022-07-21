710111cc8bc79ed528243d17f1a7d9189e1ddf14
Sean Kelly <seankwebdev@gmail.com>
2022-07-21 20:32:45 +0100

    Added about page for the project tbc at a later date

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+        <h1>About this project</h1>
+        <p style={{textAlign: "center"}}>The task tracker app is a React project built by <a href="https://github.com/sean-k-dev">Sean K</a>.</p>
+    </div>
+  ) 
+}
+export default About
