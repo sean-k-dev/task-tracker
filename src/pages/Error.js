f55696967f6ffa46b3118998ce6ab02a6574d2de
Sean Kelly <seankwebdev@gmail.com>
2022-08-02 23:09:11 +0100

    Added alt tag

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -6,7 +6,7 @@ const Error = () => {
       <div classList={styles.item}>
         <h3>HTTP 404: Did you enter the wrong URL?</h3>
         <h4>Please click <a href="/">here</a> to return to the tracker app.</h4>
-        <img src="https://images.pexels.com/photos/3752834/pexels-photo-3752834.jpeg"></img>
+        <img src="https://images.pexels.com/photos/3752834/pexels-photo-3752834.jpeg" alt="Frustrated Man"></img>
     </div>
   )
 }
