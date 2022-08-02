ac7a4d0dcc1f13ef8f53f6a4879018574da5a90e
Sean Kelly <seankwebdev@gmail.com>
2022-08-02 23:06:25 +0100

    Added more prerequisite modules and set up working JSON response to front

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express")
-const app = express.Router()
+const path = require('path')
+const app = express()
 const MongoClient = require("mongodb").MongoClient
 const cors = require("cors")
-const PORT = 8000
-const router = express.Router()
+const PORT = 5000
+// const router = express.Router()
 require("dotenv").config()
-const testAPIRouter = require("./routes/testAPI")
+// const testAPIRouter = require("./routes/api")
+const staticPath = path.join(__dirname, '/')
 
 let db,
     uri = process.env.ATLAS_URI
@@ -17,20 +19,17 @@ MongoClient.connect(uri, {useUnifiedTopology: true})
         db = client.db(dbName)    
 })
 
-app.use(express.static("public"))
+app.use(express.static('staticPath'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
 
-
-app.get("/", (req, res) => {
-    
+app.get("/api", (req, res) => {
+    res.send({message: `Backend successfully running on port ${PORT}`})
 })
-
-app.use("/testAPI", testAPIRouter)
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is now running on port ${PORT}`)
 })
 
-module.exports = router
+// module.exports = router
